3f2b99494a1794a2d0371283fe7fb7a271e7a22f
shabesh <shabeson007@gmail.com>
2019-12-12 06:23:16 +0000

    hello

---
+++ src/Area.java
@@ -0,0 +1,9 @@
+public class Area {
+    public static void main(String[] args)
+    {
+     int r=2;
+     int pi=22/7;
+     int area=pi*r*r;
+     System.out.println("the area of circle is" +" "+area+" "+"having"+" "+ r+" " +"as radius");
+    }
+}
