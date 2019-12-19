2b7c30a08f2774f7c25dbcec90282384a8a143e8
shabesh <shabeson007@gmail.com>
2019-12-19 06:11:20 +0000

    19/dec WELELL

---
+++ src/Project.java
@@ -0,0 +1,14 @@
+public class Project
+
+{
+    public static void main(String[] args) {
+        int  i,j,n;
+        n=3;
+        j=1;
+        for (i=1;i<=n;i++)
+        {
+            j=j*i;
+            System.out.println(j);
+        }
+    }
+}
